453740157676494ebd1a2bdff02a18273f03c930
ayan4m1 <andrew@bulletlogic.com>
2020-01-18 15:14:40 -0500

    replace index content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby';
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
@@ -16,8 +17,43 @@ class Index extends Component {
           <Row>
             <Col>
               <p>
-                In late 2019 I started this project as a way to give back to the
-                vaping community.
+                This site provides a resource for DIY e-liquid mixers looking
+                for flavor safety information.
+              </p>
+              <p>
+                In late 2019 /u/TeslaDelMar started working on a piece of
+                software that scans safety data sheets for known harmful
+                ingredients. A Google Sheet was created to contain the findings.
+              </p>
+              <p>
+                Shortly after, /u/Baade89 began sending emails to flavor vendors
+                and manufacturers in an effort to augment the machine-sourced
+                data.
+              </p>
+              <p>
+                The current dataset contains over 3,000 data sheets sourced from
+                17 vendors, as well as data from toxicoligsts at{' '}
+                <a
+                  href="https://www.trusticert.com/en/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  TRUSTiCERT SRL
+                </a>
+                . We scan the data sheets and query vendors for the presence of
+                32 different compounds.
+              </p>
+              <p>
+                We are in the process of migrating all of the information from
+                the{' '}
+                <a href="https://docs.google.com/spreadsheets/d/1qtol0dyNh_rvXXG9R6uvlS7qK_zC2HKfBqU_bKTbvTQ/edit?usp=sharing">
+                  Flavors to Avoid
+                </a>{' '}
+                sheet to this site.
+              </p>
+              <p>
+                For now, you can view/search the scan results{' '}
+                <Link to="/search">here</Link>.
               </p>
             </Col>
           </Row>
