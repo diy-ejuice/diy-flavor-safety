e3a4bd7af4a4f85765817e710a863c5d236d9466
ayan4m1 <andrew@bulletlogic.com>
2020-01-19 18:50:36 -0500

    create pages dynamically for vendors, flavors, and ingredients

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,96 @@
 const { resolve } = require(`path`);
 
+const createPages = async (
+  baseUrl,
+  component,
+  query,
+  slugBuilder,
+  { actions, graphql, reporter }
+) => {
+  const { createPage } = actions;
+  const result = await graphql(query);
+
+  if (result.errors) {
+    reporter.panicOnBuild('Error while running GraphQL query.');
+    return;
+  }
+
+  result.data.pageJson.nodes.forEach(node => {
+    const slug = slugBuilder(node)
+      .replace(/\s+/g, '-')
+      .toLowerCase();
+    const path = `/${baseUrl}/${slug}`;
+
+    reporter.info(`Created page for ${path}`);
+    createPage({
+      context: node,
+      component,
+      path
+    });
+  });
+};
+
+const createVendorPages = options =>
+  createPages(
+    'vendor',
+    resolve('src/pages/vendor.js'),
+    `
+{
+  pageJson: allVendorsJson {
+    nodes {
+      code
+      name
+    }
+  }
+}
+`,
+    node => `${node.code} ${node.name}`,
+    options
+  );
+
+const createIngredientPages = options =>
+  createPages(
+    'ingredient',
+    resolve('src/pages/ingredient.js'),
+    `
+{
+  pageJson: allIngredientsJson {
+    nodes {
+      name
+      casNumber
+    }
+  }
+}
+`,
+    node => node.name,
+    options
+  );
+
+const createFlavorPages = options =>
+  createPages(
+    'flavor',
+    resolve('src/pages/flavor.js'),
+    `
+{
+  pageJson: allFlavorsJson {
+    nodes {
+      name
+      vendor
+      ingredients
+    }
+  }
+}
+`,
+    node => `${node.vendor} ${node.name}`,
+    options
+  );
+
+exports.createPages = async options => {
+  await createFlavorPages(options);
+  await createVendorPages(options);
+  await createIngredientPages(options);
+};
+
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
