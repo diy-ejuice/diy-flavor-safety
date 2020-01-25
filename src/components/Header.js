95cac907a751667e98f78a134441ca53a4f2590c
ayan4m1 <andrew@bulletlogic.com>
2020-01-25 15:27:07 -0500

    add icons to all header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,13 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import { Navbar, Nav } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import {
+  faAtom,
+  faExclamationTriangle,
+  faEyeDropper,
+  faBuilding
+} from '@fortawesome/free-solid-svg-icons';
+import { faDiscord, faReddit } from '@fortawesome/free-brands-svg-icons';
 
 const Header = ({ siteTitle }) => (
   <Navbar variant="dark" bg="danger" expand="lg">
@@ -16,13 +22,13 @@ const Header = ({ siteTitle }) => (
     <Navbar.Collapse>
       <Nav>
         <Nav.Link as={Link} to="/flavors">
-          Flavors
+          <FontAwesomeIcon icon={faEyeDropper} /> Flavors
         </Nav.Link>
         <Nav.Link as={Link} to="/vendors">
-          Vendors
+          <FontAwesomeIcon icon={faBuilding} /> Vendors
         </Nav.Link>
         <Nav.Link as={Link} to="/ingredients">
-          Ingredients
+          <FontAwesomeIcon icon={faAtom} /> Ingredients
         </Nav.Link>
       </Nav>
       <Nav className="ml-auto">
@@ -31,14 +37,14 @@ const Header = ({ siteTitle }) => (
           target="_blank"
           rel="noopener noreferrer"
         >
-          /r/DIY_eJuice
+          <FontAwesomeIcon icon={faReddit} size="lg" /> /r/DIY_eJuice
         </Nav.Link>
         <Nav.Link
           href="http://link.diyejuice.org/discord"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Discord
+          <FontAwesomeIcon icon={faDiscord} size="lg" /> Discord
         </Nav.Link>
       </Nav>
     </Navbar.Collapse>
