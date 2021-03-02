44a4861bfe3b4f24fa7b569834813788b6f510e2
ayan4m1 <andrew@bulletlogic.com>
2021-03-02 14:32:26 -0500

    rewrite CategoryInfo as a functional component

--- src/components/CategoryInfo.js
+++ src/components/CategoryInfo.js
@@ -4,41 +4,37 @@ import { Badge, OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 import { getCategoryVariant } from '~utils';
 
-export default class CategoryInfo extends Component {
-  static propTypes = {
-    badgeProps: PropTypes.object,
-    category: PropTypes.string.isRequired
-  };
+export default function CategoryInfo({ badgeProps, category }) {
+  const badgeVariant = getCategoryVariant(category);
 
-  static defaultProps = {
-    badgeProps: {}
-  };
+  let textTooltip =
+    'The vape toxicity of this ingredient is currently being researched.';
 
-  render() {
-    const { badgeProps, category } = this.props;
-
-    const badgeVariant = getCategoryVariant(category);
-
-    let textTooltip =
-      'The vape toxicity of this ingredient is currently being researched.';
-
-    if (category === 'Caution') {
-      textTooltip =
-        'There is evidence that this ingredient can be toxic when vaped.';
-    } else if (category === 'Avoid') {
-      textTooltip =
-        'There is evidence that this ingredient is toxic when vaped.';
-    }
-
-    return (
-      <OverlayTrigger
-        placement="left"
-        overlay={(props) => <Tooltip {...props}>{textTooltip}</Tooltip>}
-      >
-        <Badge variant={badgeVariant} {...badgeProps}>
-          {category}
-        </Badge>
-      </OverlayTrigger>
-    );
+  if (category === 'Caution') {
+    textTooltip =
+      'There is evidence that this ingredient can be toxic when vaped.';
+  } else if (category === 'Avoid') {
+    textTooltip =
+      'There is evidence that this ingredient is toxic when vaped.';
   }
+
+  return (
+    <OverlayTrigger
+      placement="left"
+      overlay={(props) => <Tooltip {...props}>{textTooltip}</Tooltip>}
+    >
+      <Badge variant={badgeVariant} {...badgeProps}>
+        {category}
+      </Badge>
+    </OverlayTrigger>
+  );
 }
+
+CategoryInfo.propTypes = {
+  badgeProps: PropTypes.object,
+  category: PropTypes.string.isRequired
+};
+
+CategoryInfo.defaultProps = {
+  badgeProps: {}
+};
