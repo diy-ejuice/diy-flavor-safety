61c3d7ae3c7007deeabf3a5ba5ee67799c276530
ayan4m1 <andrew@bulletlogic.com>
2022-09-03 16:25:47 -0400

    add container to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faAtom,
@@ -17,48 +17,50 @@ import {
 export default function Header({ siteTitle = '' }) {
   return (
     <Navbar variant="dark" bg="danger" expand="lg">
-      <Navbar.Brand>
-        <Nav.Link as={Link} to="/">
-          <FontAwesomeIcon icon={faExclamationTriangle} /> {siteTitle}
-        </Nav.Link>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to="/flavors">
-            <FontAwesomeIcon icon={faEyeDropper} /> Flavors
+      <Container>
+        <Navbar.Brand>
+          <Nav.Link as={Link} to="/">
+            <FontAwesomeIcon icon={faExclamationTriangle} /> {siteTitle}
           </Nav.Link>
-          <Nav.Link as={Link} to="/vendors">
-            <FontAwesomeIcon icon={faBuilding} /> Vendors
-          </Nav.Link>
-          <Nav.Link as={Link} to="/ingredients">
-            <FontAwesomeIcon icon={faAtom} /> Ingredients
-          </Nav.Link>
-        </Nav>
-        <Nav>
-          <Nav.Link
-            href="https://reddit.com/r/DIY_eJuice"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faRedditAlien} size="lg" /> /r/DIY_eJuice
-          </Nav.Link>
-          <Nav.Link
-            href="http://link.diyejuice.org/discord"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faDiscord} size="lg" /> Discord
-          </Nav.Link>
-          <Nav.Link
-            href="https://github.com/diy-ejuice/diy-flavor-safety"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faGithub} size="lg" /> GitHub
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/flavors">
+              <FontAwesomeIcon icon={faEyeDropper} /> Flavors
+            </Nav.Link>
+            <Nav.Link as={Link} to="/vendors">
+              <FontAwesomeIcon icon={faBuilding} /> Vendors
+            </Nav.Link>
+            <Nav.Link as={Link} to="/ingredients">
+              <FontAwesomeIcon icon={faAtom} /> Ingredients
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link
+              href="https://reddit.com/r/DIY_eJuice"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faRedditAlien} size="lg" /> /r/DIY_eJuice
+            </Nav.Link>
+            <Nav.Link
+              href="http://link.diyejuice.org/discord"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faDiscord} size="lg" /> Discord
+            </Nav.Link>
+            <Nav.Link
+              href="https://github.com/diy-ejuice/diy-flavor-safety"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} size="lg" /> GitHub
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
